3acbf4ba87c44d5afcd870d540c049fa0da2ba32
Gabianchini <gabibisdx@gmail.com>
2024-04-09 18:04:17 -0300

    create controler and generate token function to authentication

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,41 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 
-export const login = (req,res) => {
-    res.send("loginUser");
+export const login = async (req,res) => {
+    try {
+		const {username, password} = req.body;
+		const user = await User.findOne({username});
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "" );
+
+		if (!user || !isPasswordCorrect){
+			return res.status(400).json({error:"Invalid Credentials"});
+		} else {
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePic: user.profilePic,
+		})};
+		
+	} catch (error) {
+		console.log("Error in login controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 }
 
 export const logout = (req,res) => {
-    res.send("loginUser");
+    try {
+		res.cookie("jwt","",{maxAge:0})
+		res.status(200).json({message:"Logout sucessfully"})		
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 }
 
 export const signup = async (req, res) => {
@@ -41,13 +69,25 @@ export const signup = async (req, res) => {
 			profilePic: gender === "male" ? boyProfilePic : gender === "female" ? girlProfilePic : defaultProfilePic
 		});
 
-		await newUser.save();
+		if (newUser) {
+			//generate jwt token
+			generateTokenAndSetCookie(newUser._id, res);
+			await newUser.save();
+
+			res.status(201).json({
+				_id:newUser._id,
+				fullName: newUser.fullName,
+				username: newUser.username,
+				profilePic: newUser.profilePic,
+			});
+		} else {
+				res.status(400).json({error: "Invalid user data"});
+		}
 
 	} catch (error) {
 		console.log("Error in signup controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
-
 
 };
 
