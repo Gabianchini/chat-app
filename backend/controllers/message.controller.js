f882626751bd984a778c4819157634116d12dec0
Gabianchini <gabibisdx@gmail.com>
2024-04-09 18:05:26 -0300

    create message controller and route

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,12 @@
+export const sendMessage = async (req, res) => {
+    try {
+        const {message} = req.body;
+        const {id} = req.params;
+        const senderId = 
+        
+    } catch (error) {
+        console.log("Error in sendMessage controller:", error.message)
+        res.status(500).json({message:"Internal Server Error"})
+        
+    }
+}
