a86bbc1c939f9e13b6b76c5ae6d0b66e746074fe
Gabianchini <gabibisdx@gmail.com>
2024-05-23 18:13:00 -0300

    change context folder directory

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { useAuthContext } from '../../context/AuthContext'
+import { useAuthContext } from '../context/AuthContext'
 import {toast} from "react-hot-toast";
 
 const useLogout = () => {
