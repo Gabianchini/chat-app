f0981d86670069a68f658c740111f41e2d3370b1
Gabianchini <gabibisdx@gmail.com>
2024-05-27 18:41:25 -0300

    setup project to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import  express  from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path';
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -16,18 +17,22 @@ app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res) => {
-    //root route http://localhost:5000/
-    res.send("hello world!")
-});
+const __dirname = path.resolve();
+
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+//used to serve static files
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"));
+});
+
 server.listen(PORT,() => {
     connectDb();
-    console.log(`Server Running on port ${PORT} `);
 } 
 );
 
